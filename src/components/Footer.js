7d04f990499304c8d2bd8de0e4325896c6762045
Artie <turo310@gmail.com>
2019-11-19 13:30:34 -0800

    Add new footer to Layout, add footer scss

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from "gatsby"
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer__container">
+                <p>Logo here</p>
+                <p>About</p>
+                <p>Contact us</p>
+                <p>Address</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
