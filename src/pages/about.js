83fcc6f3805dd686ea7e138d2659f4d1ed4c3ef0
Artie <turo310@gmail.com>
2019-11-19 15:10:29 -0800

    Add Services Section

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,10 +3,23 @@ import Layout from "../components/layout"
 
 const About = () =>  (
     <Layout>
-        <div>
-            <h1>This is about me!</h1>
-            <p>dscsdcdcdsn cdns cnk dsc  dskjc ksd cskc k dsck dsk csd ksck </p>
+        <div className="hero-about">
+            <div className="hero-about__text-box">
+                    <h1 className="hero-primary">
+                    <span className="hero-primary--main">About Our Passion</span>
+                </h1>
+            </div>
         </div>
+
+        <section className="section about-section">
+            <div className="about-container">
+                <p className="about-paragraph">Gatsby Detailing was established in 1993 by Arthur Creek, from his love and passion of cars. It continues today in the same tradition and commitment to excellence since day one. This passion has made Gatsby Detailing of Orange County, the finest automobile detailing company for your automobiles needs.</p>
+                <p className="about-paragraph">For over 25 years, Arthur has applied his passion, knowledge, and skills to each and every customers automobile that he has given his labor of love. He understands that a customer is not just looking for someone who can detail their car, but someone with the passion to apply that something extra, and to treat each automobile as if were his own. David takes pride in his work and he displays that commitment and skill for each customer, and the testimonials and raves only demonstrate his abilities.</p>
+                <p className="about-paragraph">The reality is that detailing a vehicle takes time and knowledge. This system combined with the products we use cannot be found at your local auto parts store or car wash.</p>
+                <p className="about-paragraph">Today’s luxury and performance cars can cost as much as a condominium or a small house and yet lose their value quickly if left unloved or mistreated. Even regularly maintained vehicles can lose their value if treated incorrectly. Gatsby Detailing protects and safeguards your investment and with regular scheduled services, we will preserve the quality of your paintwork and vehicles interior. Keeping your vehicle up with our services regularly will result in depreciation that is kept to a minimum when it’s time to sell it or trade it in.</p>
+                <p className="about-paragraph">“You're goning to love the way it looks.” - Arthur Creek</p>
+            </div>
+        </section>
     </Layout>
 )
 
--- src/pages/services.js
+++ src/pages/services.js
@@ -3,12 +3,81 @@ import Layout from "../components/layout"
 
 const Services = () =>  (
     <Layout>
-        <div>
-            <h1>Our services</h1>
-            <p>This is our services 1 </p>
-            <p>This is our services 2 </p>
-            <p>This is our services 3 </p>
+        <div className="hero-about">
+            <div className="hero-about__text-box">
+                <h1 className="hero-primary">
+                    <span className="hero-primary--main">Detailing Services</span>
+                </h1>
+            </div>
         </div>
+
+        <section className="section services__section">
+            <div className="services__container">
+                <h1>Quality Services</h1>
+                <p>Find our services below, contact us for a free consultation!</p>
+                <div className="services__grid">
+                    <div className="services__grid-item">
+                        <h2>Level 1 Detail</h2>
+                        <h3>Thorough Exterior Wash</h3>
+                        <ul>
+                            <li>Exterior wash</li>
+                            <li>Dry with 450GSM towels</li>
+                            <li>Thorough blow dry</li>
+                            <li>Wheels cleaned</li>
+                            <li>Tire Dressing with Meguiars</li>
+                        </ul>
+                        <p>Price: $60-80</p>
+                    </div>
+                    <div className="services__grid-item">
+                        <h2>Level 2 Detail</h2>
+                        <h3>Exterior and Interior</h3>
+                        <p>In addition to Level 1</p>
+                        <ul>
+                            <li>Thorough Interior Vacuum</li>
+                            <li>Thorough Wipe Down</li>
+                        </ul>
+                        <p>Price: $120-140</p>
+                    </div>
+                    <div className="services__grid-item">
+                        <h2>Level 3 Detail</h2>
+                        <h3>Detail with Added Protection</h3>
+                        <p>In addition to Level 2</p>
+                        <ul>
+                            <li>Ceramic Spray Sealant</li>
+                        </ul>
+                        <p>Price: $160-200</p>
+                    </div>
+                    <div className="services__grid-item">
+                        <h2>Level 4 Detail</h2>
+                        <h3>Light Clay Bar</h3>
+                        <p>In addition to Level 3</p>
+                        <ul>
+                            <li>Clay Bar treatment</li>
+                        </ul>
+                        <p>Price: $240-280</p>
+                    </div>
+                    <div className="services__grid-item">
+                        <h2>Level 5 Detail</h2>
+                        <h3>Decontamination with shine and wheel protection</h3>
+                        <p>In addition to Level 4</p>
+                        <ul>
+                            <li>Heavier gunk removal treatment</li>
+                            <li>Wheels coated with ceramic spray sealant</li>
+                        </ul>
+                        <p>Price: $340-400</p>
+                    </div>
+                    <div className="services__grid-item">
+                        <h2>Level 6 Detail</h2>
+                        <h3>Extra shine</h3>
+                        <p>In addition to Level 5</p>
+                        <ul>
+                            <li>Extra layer of sealant</li>
+                        </ul>
+                        <p>Price: $500-540</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     </Layout>
 )
 
