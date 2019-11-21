d0a84d496b1ec7dee6d537f6de50aa09d173a64d
Artie <turo310@gmail.com>
2019-11-21 02:14:19 -0800

    Adde gatsby web font loader to work with google fonts, optimize ferrari image

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-plugin-web-font-loader`,
+      options: {
+        google: {
+          families: ['Roboto Slab']
+        }
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sass`,
     {
