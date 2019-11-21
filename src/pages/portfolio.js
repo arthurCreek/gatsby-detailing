d905ab79b66cbd9772bf2ba5db546312dd6e48e2
Artie <turo310@gmail.com>
2019-11-21 01:48:24 -0800

    Add image files, add image gallery to portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Layout from "../components/layout"
+import Img from 'gatsby-image';
 
-const Portfolio = () =>  (
+const Portfolio = (props) =>  (
     <Layout>
         <div className="hero-sub">
             <div className="hero-sub__text-box">
@@ -11,13 +12,93 @@ const Portfolio = () =>  (
             </div>
         </div>
 
-        <div className="section section-portfolio">
-            <div className="portfolio-container">
-                <h1>This is our Portfolio!</h1>
-                <p>Images here and testimonials as well! </p>
-            </div>
-        </div>
+        <section class="section-cars">
+            <ul class="car-showcase">
+                <li>
+                    <figure class="car-photo">
+                        <Img fluid={props.data.aston.childImageSharp.fluid} className="car-image" alt="Korean bibimbap with egg and vegetables" />
+                    </figure>
+                </li>
+                <li>
+                    <figure class="car-photo">
+                        <Img fluid={props.data.bmw.childImageSharp.fluid} className="car-image" alt="Simple italian pizza with cherry tomatoes" />
+                    </figure>
+                </li>
+                <li>
+                    <figure class="car-photo">
+                        <Img fluid={props.data.zanetti.childImageSharp.fluid} className="car-image" alt="Chicken breast steak with vegetables" />
+                    </figure>
+                </li>
+                <li>
+                    <figure class="car-photo">
+                        <Img fluid={props.data.gtr.childImageSharp.fluid} className="car-image" alt="Autumn pumpkin soup" />
+                    </figure>
+                </li>
+            </ul>
+
+            <ul class="car-showcase">
+                <li>
+                    <figure class="car-photo">
+                        <Img fluid={props.data.lambo.childImageSharp.fluid} className="car-image" alt="Paleo beef steak with vegetables" />
+                    </figure>
+                </li>
+                <li>
+                    <figure class="car-photo">
+                        <Img fluid={props.data.classic.childImageSharp.fluid} className="car-image" alt="Healthy baguette with egg and vegetables" />
+                    </figure>
+                </li>
+                <li>
+                    <figure class="car-photo">
+                        <Img fluid={props.data.classic_mercedez.childImageSharp.fluid} className="car-image" alt="Burger with cheddar and bacon" />
+                    </figure>
+                </li>
+                <li>
+                    <figure class="car-photo">
+                        <Img fluid={props.data.lowrider.childImageSharp.fluid} className="car-image" alt="Granola with cherries and strawberries" />
+                    </figure>
+                </li>
+            </ul>
+        </section>
     </Layout>
 )
 
 export default Portfolio;
+
+export const fluidImage = graphql`
+fragment fluidImage on File {
+  childImageSharp {
+    fluid(maxWidth: 1000) {
+      ...GatsbyImageSharpFluid
+    }
+  }
+}
+`;
+
+export const pageQuery = graphql`
+  query {
+    aston: file(relativePath: { eq: "aston.jpg" }) {
+      ...fluidImage
+    }
+    bmw: file(relativePath: { eq: "bmw.jpg" }) {
+      ...fluidImage
+    }
+    classic: file(relativePath: { eq: "classic.jpg" }) {
+      ...fluidImage
+    }
+    zanetti: file(relativePath: { eq: "zanetti.jpg" }) {
+      ...fluidImage
+    }
+    classic_mercedez: file(relativePath: { eq: "classic_mercedez.jpg" }) {
+      ...fluidImage
+    }
+    lambo: file(relativePath: { eq: "lambo.jpg" }) {
+      ...fluidImage
+    }
+    lowrider: file(relativePath: { eq: "lowrider.jpg" }) {
+      ...fluidImage
+    }
+    gtr: file(relativePath: { eq: "gtr.jpg" }) {
+      ...fluidImage
+    }
+  }
+`
