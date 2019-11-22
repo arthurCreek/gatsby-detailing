27b76e6a1af051264cdcee221ccda38eacd83db7
Artie <turo310@gmail.com>
2019-11-22 02:37:19 -0800

    Change import path in layout for footer and header

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,8 +8,8 @@
 import React from "react"
 import PropTypes from "prop-types";
 
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "../components/header";
+import Footer from "../components/footer";
 import "../styles/styles.scss";
 
 const Layout = ({ children }) => {
