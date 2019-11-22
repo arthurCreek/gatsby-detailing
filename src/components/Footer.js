87dcb9767aeb44f429e17d93c1a1eeed71105c4a
Artie <turo310@gmail.com>
2019-11-21 16:14:31 -0800

    Add footer text and layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer__container">
-                <p>Logo here</p>
-                <p>About</p>
-                <p>Contact us</p>
-                <p>Address</p>
+                <div className="footer__container--link-credit">
+                    <div className="footer__nav">
+                        <ul>
+                            <li><a href="#">About us</a></li>
+                            <li><a href="#">Services</a></li>
+                            <li><a href="#">Portfolio</a></li>
+                            <li><a href="#">FAQs</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div className="footer__credits">
+                        Icons made by 
+                        <a href="https://www.flaticon.com/authors/vectors-market" title="Vectors Market"> Vectors Market </a>
+                        and
+                        <a href="https://www.flaticon.com/authors/freepik" title="Freepik"> Freepik </a>  
+                        from 
+                        <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+                    </div>
+                </div>
 
-                <div>
-                    Icons made by 
-                    <a href="https://www.flaticon.com/authors/vectors-market" title="Vectors Market">Vectors Market</a> 
-                    from 
-                    <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-                 </div>
-                 <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+                <div className="footer__copywrite">
+                    <p>
+                        Copyright &copy; 2019 by Arturo Arroyo. All rights reserved.
+                    </p>
+                </div>
             </div>
 
         </footer>
