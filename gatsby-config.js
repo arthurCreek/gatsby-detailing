d03f21b95edf715c05a2a201d9cc08b8ca5d7ecd
Artie <turo310@gmail.com>
2019-11-21 22:51:33 -0800

    Add nav bar effects, logos, and hide menu when certain screen size is viewing

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Detailing Services`,
+    description: `Gatsby Detaling provides proffesional cleaning services.`,
+    author: `Arturo Arroyo`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,13 +27,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `gatsby-detailing`,
+        short_name: `detailing`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-detail-logo.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,25 @@
-import React from 'react'
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from "gatsby";
+import logo from '../images/gatsby-detail-logo.png';
+import menu_logo from '../images/menu-button.png';
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className="header">
-            <ul className="header__list">
-                <li><Link className="header__list-item" to="/">Home</Link></li>
-                <li><Link className="header__list-item" to="/about/">About Us</Link></li>
-                <li><Link className="header__list-item" to="/services/">Services</Link></li>
-                <li><Link className="header__list-item" to="/portfolio/">Portfolio</Link></li>
-                <li><Link className="header__list-item" to="/faq/">FAQ</Link></li>
-                <li><Link className="header__list-item" to="/contact_us/">Contact Us</Link></li>
-
-            </ul>
+            <div className="header__container">
+                <li id="header-logo"><img src={logo} className="logo"/></li>
+                <img src={menu_logo} alt="menu icon" className="header__menu-logo"/>
+                <nav className="header__nav">
+                    <ul className="header__list">
+                        <li><Link className="header__list-item" to="/">Home</Link></li>
+                        <li><Link className="header__list-item" to="/about/">About Us</Link></li>
+                        <li><Link className="header__list-item" to="/services/">Services</Link></li>
+                        <li><Link className="header__list-item" to="/portfolio/">Portfolio</Link></li>
+                        <li><Link className="header__list-item" to="/faq/">FAQ</Link></li>
+                    </ul>
+                </nav>
+                <Link className="header__contact " to="/contact_us/">Contact Us</Link>
+            </div>
         </header>
     )
 }
