c3ea355ceb6af8d3ba11c634678c660c9d4036eb
Artie <turo310@gmail.com>
2019-11-22 02:16:59 -0800

    Issue with layout.js importing footer and not Footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,8 +8,8 @@
 import React from "react"
 import PropTypes from "prop-types";
 
-import Header from "./header";
-import Footer from "./footer";
+import Header from "./Header";
+import Footer from "./Footer";
 import "../styles/styles.scss";
 
 const Layout = ({ children }) => {
